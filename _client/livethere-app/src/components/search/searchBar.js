8bccf1c7629860c046d0422cb2bfa74ad60fac3a
rsongdalsong <jeongwon412@gmail.com>
2020-07-30 01:52:34 -0400

    Fixed searchBar.js: select dropdown using keyboard, on/off dopdown onBlur/click

--- _client/livethere-app/src/components/search/searchBar.js
+++ _client/livethere-app/src/components/search/searchBar.js
@@ -4,12 +4,17 @@ import {IconButton} from '@material-ui/core';
 import SearchIcon from '@material-ui/icons/Search';
 import * as levenshtein from 'js-levenshtein'
 
+// TODO: WRITE MORE COMMENTS.
 // TODO: Componentize search component
 const Search = (props) => {
+  /* state and setState*/
   const [keyword, setKeyword] = useState('');
   const [flatDict, setFlatDict] = useState([]);
   const [matchingTuples, setMatchingTuples] = useState([]);
+  const [dropDownIndx, setDropDownIndx] = useState(-1);
+  const [showDropbox, setShowDropbox] = useState(false);
 
+  /* hooks */
   useEffect(() => {
     setFlatDict(Object.entries(props.dictToSearch));
   }, [props.dictToSearch])
@@ -20,6 +25,7 @@ const Search = (props) => {
 
 
   // TODO: Make it generic for future usage
+  /* utils */
   const searchKeyword = () => {
     if (!keyword || keyword === '') {
       setMatchingTuples([]);
@@ -56,28 +62,74 @@ const Search = (props) => {
     setMatchingTuples(keys);
   }
 
+  const selectDropdown = (index) => {
+    // Wrapper of setDropDownIndx with index condition.
+    if (index >= 0 && index < matchingTuples.length)
+      setDropDownIndx(index);
+  }
+
+  /* event handler */
+  const handleSearchByKeyword = (word) => {
+    if (keyword !== word) setKeyword(word);    
+    if (matchingTuples && matchingTuples.length > 0){
+      const [key, val] = matchingTuples[0]
+      props.handleSearch(key);
+    }
+    else{
+      alert('No University was found!');
+    }
+  }
+
+  const handleSearchByKey = (key) =>{
+    props.handleSearch(key);
+  }
+
   const handleInputChange = (event) => {
+    setShowDropbox(true);
     setKeyword(event.target.value);
+  }
+
+  const handleOnBlur = () => {
+    // OnBlur reset matching tuple and dropDownIndx.
+    setShowDropbox(false);
+    setDropDownIndx(-1);
   }
 
   const handleKeyPress = (event) => {
     if (event.key === 'Enter') {
-      props.handleSearch(keyword);
+      if (dropDownIndx !== -1 && showDropbox){
+        // If dropDownIndx is selected and dropbox is shown => search by dropbox index.
+        const [key, val] = matchingTuples[dropDownIndx];
+        handleSearchByKey(key);
+      }
+      else{
+        // If dropdown index is not selected or drop box is not shown, search by keyword
+        handleSearchByKeyword(keyword);
+      }
+    }
+    if (event.key ==='ArrowUp'){
+      selectDropdown(dropDownIndx-1);
+    }
+    if (event.key === 'ArrowDown'){
+      selectDropdown(dropDownIndx+1);
     }
   }
 
-  const dropdowns = () => {
-    return (matchingTuples && matchingTuples.length > 0) ?
-      <div className="popup">
+  // Dropdown inner component. 
+  const Dropdown = (props) => {
+    return (showDropbox && matchingTuples && matchingTuples.length > 0) ?
+      <div className="popup" onClick={()=>{console.log(1)}}>
         <div className="popup-content">
           {
-            matchingTuples.map(([key, val]) => (
-              <div className='popup-item'
-                   key={key}
-                   onClick={() => props.handleSearch(key)}>
-                {`${val.universityName}, at ${val.campus} `}
-              </div>
-            ))
+            matchingTuples.map(([key, val], i) => {
+              return (
+                <div className={dropDownIndx === i ? 'popup-item:hover' : 'popup-item'}
+                     key={key}
+                     onClick={() => handleSearchByKey(key)}>
+                  {`${val.universityName}, at ${val.campus} `}
+                </div>
+              )
+            })
           }
         </div>
       </div>
@@ -94,15 +146,16 @@ const Search = (props) => {
             placeholder="Search your university/college"
             onChange={handleInputChange}
             onKeyUp={handleKeyPress}
+            onBlur={handleOnBlur}
+            onClick={()=>{ if (keyword !== '') setShowDropbox(true)}}
           />
           <IconButton id='searchButton'
-                      onClick={() => props.handleSearch(keyword)}
+                      onClick={() => handleSearchByKeyword(keyword)}
                       size='small'>
             <SearchIcon/>
           </IconButton>
         </div>
-        {/* TODO: Handle keyboard input to choose*/}
-        {dropdowns()}
+        <Dropdown/>
       </div>
     </>
   );
