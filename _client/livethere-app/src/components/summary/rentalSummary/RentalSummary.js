7e4f79d82ee9fdf1121f24c354e3db7a707aaa5c
hpark0811 <hpark327@uwo.ca>
2020-08-14 14:18:41 -0400

    add responsive rental overview component

---
+++ _client/livethere-app/src/components/summary/rentalSummary/RentalSummary.js
@@ -0,0 +1,151 @@
+import React, {useEffect, useState} from 'react';
+import axios from 'axios'
+import HouseRoundedIcon from '@material-ui/icons/HouseRounded';
+import styles from "./RentalSummary.module.scss";
+import {FormControl, FormControlLabel, FormLabel, FormGroup, Checkbox} from '@material-ui/core';
+import RadioGroup from "@material-ui/core/RadioGroup";
+import Radio from "@material-ui/core/Radio";
+import NativeSelect from "@material-ui/core/NativeSelect";
+import SummaryLayout from "../../layout/summary/SummaryLayout";
+import Grid from "@material-ui/core/Grid";
+import Button from "@material-ui/core/Button";
+
+const RentalSummary = (props) => {
+  const [maxDistance, setMaxDistance] = useState(15);
+  const [propertyType, setPropertyType] = useState('');
+  const [bathCount, setBathCount] = useState(null);
+  const [bedCount, setBedCount] = useState(null);
+  const [summary, setSummary] = useState()
+
+  useEffect(() => {
+    fetchRentalSummary()
+  }, [props]);
+
+  const fetchRentalSummary = () => {
+    let params = {universityId: props.universityId};
+
+    if (maxDistance) {
+      params.maxDistance = maxDistance;
+    }
+    if (propertyType) {
+      params.propertyType = propertyType;
+    }
+    if (bathCount) {
+      params.bathCount = bathCount;
+    }
+    if (bedCount) {
+      params.bedCount = bedCount;
+    }
+
+    axios.get(
+      `http://localhost:5000/rental/average`,
+      {
+        params: params
+      })
+      .then(response => {
+        setSummary(response.data);
+      })
+      .catch(error => {
+        console.error(error);
+      })
+  }
+
+  const form = (
+    <>
+      <Grid container>
+        <Grid item
+              sm={4}
+              xs={6}>
+          <div>Includes:</div>
+          <FormControl component="fieldset">
+            <RadioGroup aria-label="propertyType"
+                        name="propertyType"
+                        value={propertyType}
+                        onChange={event => setPropertyType(event.target.value)}>
+              {['condo', 'house', 'town house', 'bachelor'].map((type) => (
+                <FormControlLabel value={type}
+                                  control={<Radio color="primary"/>}
+                                  label={type}/>
+              ))}
+              <FormControlLabel value={""}
+                                control={<Radio color="primary"/>}
+                                label={'N/A'}/>
+            </RadioGroup>
+          </FormControl>
+        </Grid>
+        <Grid container
+              item
+              sm={8}
+              xs={6}>
+          <Grid item
+                xs={12}
+                sm={6}>
+            <div>Distance:</div>
+            <FormControl className={styles.Dropdown}>
+              <NativeSelect value={maxDistance}
+                            onChange={event => setMaxDistance(parseInt(event.target.value))}>
+                {
+                  [15, 10, 7, 5, 3, 1].map((maxDistance) => (
+                    <option value={maxDistance}>{'< ' + maxDistance + 'km'}</option>
+                  ))
+                }
+              </NativeSelect>
+            </FormControl>
+          </Grid>
+          <Grid item
+                xs={12}
+                sm={6}>
+            <div>Bathrooms:</div>
+            <FormControl className={styles.Dropdown}>
+              <NativeSelect value={bathCount}
+                            onChange={event => setBathCount(parseInt(event.currentTarget.value))}>
+                <option value={null}>All</option>
+                {
+                  [5, 4, 3, 2, 1].map((bathCount) => (
+                    <option value={bathCount}>{bathCount}</option>
+                  ))
+                }
+              </NativeSelect>
+            </FormControl>
+          </Grid>
+          <Grid item
+                xs={12}
+                sm={6}>
+            <div>Bedrooms:</div>
+            <FormControl className={styles.Dropdown}>
+              <NativeSelect value={bedCount}
+                            onChange={event => setBedCount(parseInt(event.currentTarget.value))}>
+                <option value={null}>All</option>
+                {
+                  [5, 4, 3, 2, 1].map((bedCount) => (
+                    <option value={bedCount}>{bedCount}</option>
+                  ))
+                }
+              </NativeSelect>
+            </FormControl>
+          </Grid>
+        </Grid>
+      </Grid>
+    </>
+  )
+
+  const summaryText = (
+    summary && summary.rentalsCount > 0 ? (
+      <div>
+        <div>Average rental price is <b>${summary.average?.toFixed(0)}/mo per
+          room</b></div>
+        <div>Calculated with <b>{summary.rentalsCount}</b> listings found online</div>
+      </div>
+    ) : <div>No listings found!</div>
+  )
+
+  return (
+    <SummaryLayout icon={<HouseRoundedIcon/>}
+                   iconText={'RENTAL'}
+                   formElement={form}
+                   onSubmit={fetchRentalSummary}
+                   summaryElement={summaryText}/>
+  )
+}
+
+export default RentalSummary;
--- _client/livethere-app/src/pages/costOfLiving/overview/Overview.js
+++ _client/livethere-app/src/pages/costOfLiving/overview/Overview.js
@@ -1,24 +1,36 @@
-import React, {useEffect} from "react";
+import React, {useEffect, useState} from "react";
 import SimpleMap from "../../../components/map/Map";
 import styles from "./Overview.module.scss";
+import axios from "axios";
+import RentalSummary from "../../../components/summary/rentalSummary/RentalSummary";
 
 const Overview = (props) => {
+  const [universityDetail, setUniversityDetail] = useState();
+
   useEffect(() => {
     const { match: { params } } = props;
 
     console.log('Fetch cost of living overview data with university id: ' + params.id);
-  }, [])
+    axios.get(`http://localhost:5000/university/${params.id}`)
+      .then(response => {
+        setUniversityDetail(response.data);
+      })
+      .catch(error => {
+        console.error(error);
+      })
 
-  return (
+  }, [props])
+
+  return universityDetail ?
     <div className={styles.overviewContainer}>
       <div className={styles.mapContainer}>
-        <SimpleMap/>
+        <SimpleMap center={{lat: universityDetail.latitude, lng: universityDetail.longitude}}/>
       </div>
-      <div>
-        Overview descriptions
+      <div className={styles.listContainer}>
+        <RentalSummary universityId={universityDetail.id}/>
       </div>
     </div>
-  )
+    : null;
 }
 
 export default Overview;
