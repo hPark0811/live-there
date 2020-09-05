9db48e8cc3b29cbcdec81c568a3d3388135fd9f8
rsongdalsong <jeongwon412@gmail.com>
2020-09-05 16:13:15 -0400

    Combined filter baed api and prediction api
    fixed rental filter issues

--- _server/api/rental.py
+++ _server/api/rental.py
@@ -6,6 +6,7 @@ import numpy as np
 import pickle
 import pgeocode
 import os
+from itertools import product
 
 
 # CONST
@@ -32,95 +33,61 @@ def get_rentals():
     return rentals_schema.jsonify(all_rentals)
 
 
-# Return predicted rental price.
-@rental_api.route('/predict', methods=['GET'])
+@rental_api.route('/summary', methods=['GET'])
 @cache.cached(timeout=86400, key_prefix=make_cache_key)
-def predict_rental():
-    valid_args = [
-        request.args.get('universityId') is not None,
-        request.args.get('bathCount') is not None,
-        request.args.get('bedCount') is not None,
-        request.args.get('propertyType') is not None,
-        request.args.get('postalCode') is not None
+def get_rental_summary():
+    """ Return rental summary.
+
+    Args:
+        universityId: university Id. If none -> raise BadRequest.
+        postalCode: postal code used to get locational data from pgeocode. If none -> raise BadRequest. 
+        maxDistance: radius of outer range. (default 15)
+        minDistance: radius of inner range. (default 0)
+        bedCount: number of bedroom.
+            if not provided, 
+                average metric: does not filter based on bedroom count.
+                prediction metric: (weighted in future) average of predictions over bedCounts [1, 2, 3, 4, 5].
+        bathCount: number of bedroom.
+            if not provided, 
+                average metric: does not filter based on bathroom count.
+                prediction metric: (weighted in future) average of predictions over bathCounts [1, 2, 3, 4, 5].
+        propertyType: type of the property.
+            if not provided,
+                average metric: does not filter based on propertyType.
+    Returns:
+        rentalCounts:
+            average: number of rents that were used to calculate average.
+            prediction: -1
+        metrics: average(filter based) or prediction(random forest). (default average)
+        summary: average/prediction of rental prices.
+            If there is no rentals data -> prediction baed on random forest.
+            If there is no rentals and propertyType is not provided, -1.       
+        distance: maxDistance.
+    """
+    # TODO: Break this function to parts.
+
+    # Check for invalid arguments.
+    invalid_args = [
+        request.args.get('universityId') is None,
+        request.args.get('postalCode') is None
     ]
 
-    if not all(valid_args):
-        raise BadRequest('Must provide valid arguments')
+    if any(invalid_args):
+        raise BadRequest('Must provide universityId and postalCode')
 
-    # Retrieve county.
-    nomi = pgeocode.Nominatim('ca')
-    location_data = nomi.query_postal_code(
-        [request.args.get('postalCode')]).to_dict('records')[0]
-    county = location_data['county_name']
+    """ Filter baed average starts here by default. """
 
-    # Load model.
-    with open(os.path.join(ML_MODEL_PATH, 'model.pkl'), 'rb') as f:
-        rf_model = pickle.load(f)
+    # Default min/max distance.
+    min_distance = 0
+    max_distance = 15
 
-    # Load scalers.
-    # TODO: only single one hot enocoding.
-    with open(os.path.join(ML_MODEL_PATH, 'property_one_hot.pkl'), 'rb') as f:
-        property_one_hot = pickle.load(f)
-
-    with open(os.path.join(ML_MODEL_PATH, 'county_one_hot.pkl'), 'rb') as f:
-        county_one_hot = pickle.load(f)
-
-    with open(os.path.join(ML_MODEL_PATH, 'normalization.pkl'), 'rb') as f:
-        scaler = pickle.load(f)
-
-    # Average all the alias
-    alias = PROPERTY_ALIAS_MAP[request.args.get('propertyType')]
-    predictions = []
-
-    # For each property type in alias, predict using RF model.
-    for p in alias:
-        # Normalize features.
-        features = scaler.transform(np.array([int(request.args.get('bathCount')), int(
-            request.args.get('bedCount'))]).reshape(1, -1))
-
-        # One hot encode property and location.
-        p_sparse = property_one_hot.transform(
-            np.array([p]).reshape(1, -1)).toarray()
-        c_sparse = county_one_hot.transform(
-            np.array([county]).reshape(1, -1)).toarray()
-
-        x = np.concatenate([features, p_sparse, c_sparse], axis=1)
-
-        # Predict.
-        predictions.append(rf_model.predict(x).item())
-
-    return {
-        'prediction': np.mean(predictions)
-    }
-
-
-@rental_api.route('/average', methods=['GET'])
-@cache.cached(timeout=86400, key_prefix=make_cache_key)
-def get_average_rental():
-    if not request.args.get('universityId'):
-        raise BadRequest('University ID must not be null')
-    # default parameters
-    min_distance_km = 0
-    max_distance_km = 10
-    property_types = None
-    bed_count = 0
-    bath_count = 0
-
-    # TODO: Filter out edge cases
     if request.args.get('minDistance'):
-        min_distance_km = request.args['minDistance']
-    if request.args.get('maxDistance'):
-        max_distance_km = request.args['maxDistance']
-    if request.args.get('propertyType'):
-        property_types = PROPERTY_ALIAS_MAP[
-            request.args.get('propertyType')
-        ]
-    if request.args.get('bathCount'):
-        bath_count = request.args.get('bathCount')
-    if request.args.get('bedCount'):
-        bed_count = request.args.get('bedCount')
+        min_distance = int(request.args.get('minDistance'))
+    
+    if request.args.get('maxDistnace'):
+        max_distance = int(request.args.get('maxDistance'))
 
-    # Querying DB
+    # Querying DB & filter based on min/max radius and universityId.
     queried_rentals = db.session.query(
         Rental.rentalPrice,
         Rental.bathroomCount,
@@ -132,35 +99,127 @@ def get_average_rental():
     ).filter(
         RentalRange.universityId == request.args['universityId']
     ).filter(
-        RentalRange.rentToUniversityDistance >= min_distance_km
+        RentalRange.rentToUniversityDistance >= min_distance
     ).filter(
-        RentalRange.rentToUniversityDistance <= max_distance_km
-    )
+        RentalRange.rentToUniversityDistance <= max_distance
+    ) 
 
-    if property_types:
+    # Filter baed on propertyType, bathCount, bedCount. If none, automatically unfiltered.
+    if request.args.get('propertyType') is not None:
         queried_rentals = queried_rentals.filter(
-            Rental.propertyType.in_(property_types))
-    if bath_count:
+            Rental.propertyType.in_(PROPERTY_ALIAS_MAP[request.args.get('propertyType')])
+        )        
+    if request.args.get('bathCount') is not None:
         queried_rentals = queried_rentals.filter(
-            Rental.bathroomCount == bath_count)
-    if bed_count:
+            Rental.bathroomCount == request.args.get('bathCount')
+        )
+    if request.args.get('bedCount') is not None:
         queried_rentals = queried_rentals.filter(
-            Rental.bedroomCount == bed_count)
-
+            Rental.bedroomCount == request.args.get('bedCount')
+        )
     queried_rentals = queried_rentals.all()
 
-    # Calculating average average
+    # Calculating average.
     rentals_count = len(queried_rentals)
-    rentals_average = calculate_average_rent_per_room(queried_rentals)
 
-    return {
-        'rentalsCount': rentals_count,
-        'average': rentals_average,
-        'distance': max_distance_km
-    }
+    # Check if valid rental average.
+    valid_avg = [
+        rentals_count > 0,
+    ]
+
+    print(request.args.get('propertyType'), request.args.get('bathCount'), request.args.get('bedCount'), rentals_count, max_distance, min_distance)
+    if all(valid_avg):
+        rentals_average = calculate_average_rent_per_room(queried_rentals)
+        # Successfully calculated filtered average.
+        return {
+            'rentalsCount': int(rentals_count),
+            'summary': float(rentals_average),
+            'distance': int(max_distance),
+            'metric': 'average'
+        }
+
+    # Check if prediction is available.
+    prd_valid = [
+        request.args.get('propertyType') is not None,
+        # request.args.get('bedCount') is not None,
+        # request.args.get('bathCount') is not None
+    ]
+    
+    """ Prediction starts here """    
+    if (not all(valid_avg)) and all(prd_valid):
+        # Retrieve county from pgeocode.
+        nomi = pgeocode.Nominatim('ca')
+        location_data = nomi.query_postal_code([request.args.get('postalCode')]).to_dict('records')[0]
+        county = location_data['county_name']
+
+        """ Load pickles. """
+        with open(os.path.join(ML_MODEL_PATH, 'model.pkl'), 'rb') as f:
+            rf_model = pickle.load(f)
+
+        # TODO: only single one hot enocoding.
+        with open(os.path.join(ML_MODEL_PATH, 'property_one_hot.pkl'), 'rb') as f:
+            property_one_hot = pickle.load(f)
+
+        with open(os.path.join(ML_MODEL_PATH, 'county_one_hot.pkl'), 'rb') as f:
+            county_one_hot = pickle.load(f)
+
+        with open(os.path.join(ML_MODEL_PATH, 'normalization.pkl'), 'rb') as f:
+            scaler = pickle.load(f)
+
+        # Average all the alias, bedRanges, bathRanges.
+        alias = PROPERTY_ALIAS_MAP[request.args.get('propertyType')]
+        bedRange = [1, 2, 3, 4, 5]
+        bathRange = [1, 2, 3]
+
+        if request.args.get('bedCount') is not None:
+            bedRange = [int(request.args.get('bedCount'))]
+
+        if request.args.get('bathCount') is not None:
+            bathRange = [int(request.args.get('bathCount'))]
+
+        predictions = []
+        aggregator = product(alias, bedRange, bathRange)        
+
+        for p, bedCount, bathCount in aggregator:
+            # Normalize features.
+            features = scaler.transform(np.array([bathCount, bedCount]).reshape(1, -1))
+
+            # One hot encode property and location.
+            p_sparse = property_one_hot.transform(np.array([p]).reshape(1, -1)).toarray()
+            c_sparse = county_one_hot.transform(np.array([county]).reshape(1, -1)).toarray()
+
+            x = np.concatenate([features, p_sparse, c_sparse], axis=1)
+
+            # Predict.
+            predictions.append(rf_model.predict(x).item())
+        
+        # TODO: Instead of summary <- mean(predictions) must be weighted average based on probability distribution for each alias, bathRange, bedCount.
+        if request.args.get('bedCount') is None:
+            # Considering all of bedCounts have probability distribution. 
+            pdf = np.array([0.2, 0.2, 0.2, 0.2, 0.2])
+            divisor = np.dot(pdf, np.array(bedRange))
+        else:
+            divisor = float(request.args.get('bedCount'))
+        
+        # Successfully calculated rental predictions.
+        return {
+            'rentalsCount': -1,
+            'summary':  np.mean(predictions)/divisor,
+            'distance': -1,
+            'metric': 'prediction'
+        }
+    else:
+        # Unavaiable to provide meaningful summary.
+        return {
+            'rentalsCount': 0,
+            'summary': -1,
+            'distance': int(max_distance),
+            'metric': 'average'
+        }
 
 
 def calculate_average_rent_per_room(rents):
+    # TODO: accounts for NaN values, and fix format.
     """ calculates average price of rent per room
 
     Args:
@@ -180,6 +239,7 @@ def calculate_average_rent_per_room(rents):
 
 
 def calculate_mean(nums):
+    # TODO: accounts for NaN values, and fix format.
     """ calculates mean with outliers removed
 
     Args:
